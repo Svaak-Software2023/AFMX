499526357604b8eb5802d16efd2632290a69dc0a
Your New Name <riteshpandey7356@gmail.com>
2023-12-08 18:29:18 +0530

    complain portal

--- src/pages/complainPortal/ComplainPortal.jsx
+++ src/pages/complainPortal/ComplainPortal.jsx
@@ -1,68 +1,19 @@
-import { RiArrowDropDownLine } from "react-icons/ri";
+import { useEffect, useState } from "react";
+import ExistingClients from "./ExistingClients";
+import NonExistingClients from "./NonExistingClients";
 import "./complain.css";
-import { TextField } from "@mui/material";
-import { BsFillPersonFill } from "react-icons/bs";
-import { Link } from "react-router-dom";
-
 function ComplainPortal() {
+  const [data,setData]=useState(false)
+  
   return (
     <>
-      <div className="container p-0 my-3 bg-white">
-        <div className="row m-0 p-0 ">
-          <div className="col-lg-3 col-md-3 col-0 p-0 service_menu">
-            <h3 className="left-menu-heading">
-              Complain System
-            </h3>
-            <div className="left-menu mb-3">
-              <div className="left-menu-user-profile">
-                <div className="side-menu-profile-img">
-                  <img src="https://img.freepik.com/premium-vector/man-avatar-profile-picture-vector-illustration_268834-538.jpg" alt="" className="img-fluid" />
-                </div>
-                <div className="left-menu-user-profile-name">
-                  <h3 >User Name</h3>
-                </div>
-              </div>
-              <h3 className="MAIN-NAVIGATION">MAIN NAVIGATION</h3>
-              <ul className="left-menu-list">
-                <li><Link to="">Dashboard</Link></li>
-                <li><Link to="">Complaint <RiArrowDropDownLine className="fs-5" /></Link></li>
-                <li><Link to="">Reports</Link></li>
-                <li></li>
-              </ul>
-            </div>
-          </div>
-          <div className=" col-lg-9 col-md-9 col-12 service_content" style={{paddingLeft:"2px",paddingRight:"0px"}}>
-            <h3 className="left-menu-heading text-end fs-6" >
-              User Name
-            </h3>
-            <div>
-            </div>
-            <div className="p-3">
-              <h3 className="heading border-none">Dashboard</h3>
-              <div className="row mt-3">
-                <div className="col-lg-4 col-12 mb-3">
-                  <div className="box-item1">
-                    <h3 className="box-item-text">150</h3>
-                    <h3 className="box-item-text box-item-textb-bottom">Total Complaints</h3>
-                  </div>
-                </div>
-                <div className="col-lg-4 col-12 mb-3">
-                  <div className="box-item2">
-                  <h3 className="box-item-text">150</h3>
-                    <h3 className="box-item-text box-item-textb-bottom">Complaint Solve</h3>
-                  </div>
-                </div>
-                <div className="col-lg-4 col-12 mb-3">
-                  <div className="box-item3">
-                  <h3 className="box-item-text">150</h3>
-                    <h3 className="box-item-text box-item-textb-bottom">Reports</h3>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
+      {
+        data
+          ?
+          <ExistingClients />
+          :
+          <NonExistingClients />
+      }
     </>
   );
 }
---
+++ src/pages/complainPortal/NonExistingClients.jsx
@@ -0,0 +1,120 @@
+import React from 'react'
+import ImportantLinks from "../../components/ImportantLinks/ImportantLinks";
+import { useState } from "react";
+
+function NonExistingClients() {
+    const [file, setFile] = useState("")
+    const initialValue={ complain_type:null, name: "", phone: "", category: "", service: "", description: "" }
+    const [formData, setFormData] = useState(initialValue)
+  
+    const handleChange = (e) => {
+      setFormData({ ...formData, [e.target.name]: e.target.value })
+    }
+  
+    const handleSubmit = (e) => {
+      e.preventDefault()
+      formData.file = file
+      console.log(formData);
+      setFormData(initialValue)
+      setFile("")
+    }
+    
+  return (
+    <>
+    <div className="container p-0 my-3 ">
+      <div className="row m-0 p-0 ">
+        <div className="col-lg-3 col-md-3 col-0 p-0 service_menu">
+          <ImportantLinks />
+        </div>
+        <div className=" col-lg-9 col-md-9 p-0 col-12 service_content bg-white">
+          <h3 className="left-menu-heading fs-6" >
+            Complaint
+          </h3>
+          <div>
+          </div>
+          <div className="p-3">
+            <h3 className="heading fs-6 mb-4">Add Complaint</h3>
+            <div className="row mt-3 px-lg-3">
+              <form onSubmit={handleSubmit}>
+
+                <div className="row mb-3">
+                  <div className="col-lg-6 col-12">
+                    <div className="complain-radio justify-content-end m-lg-3 mb-3">
+                      <input type="radio" id="radio1" name="complain_type" value="Complaint of Fleet" onChange={handleChange}/>
+                      <label htmlFor="radio1">Complaint of Fleet</label>
+                    </div>
+                  </div>
+                  <div className="col-lg-6 col-12">
+                    <div className="complain-radio m-lg-3 mb-3">
+                      <input type="radio" id="radio2" name="complain_type" value="Complaint of Driver" onChange={handleChange}/>
+                      <label htmlFor="radio2">Complaint of Driver</label>
+                    </div>
+                  </div>
+                </div>
+
+                <div className="row">
+                  <div className="col-lg-6">
+                    <h3 className="complain-input-lebel">Name :</h3>
+                    <div className="mb-3 complain-input">
+                      <input type="text" className="form-control shadow-none complain-input" onChange={handleChange} value={formData.name} name="name" placeholder="Complaint Name ..." />
+                    </div>
+                  </div>
+                  <div className="col-lg-6">
+                    <h3 className="complain-input-lebel">Phone :</h3>
+                    <div className="mb-3 complain-input">
+                      <input type="text" className="form-control shadow-none complain-input" onChange={handleChange} value={formData.phone} name="phone" placeholder="Phone Number ..." />
+                    </div>
+                  </div>
+                </div>
+                <div className="row">
+
+                  <div className="col lg-6 col-12">
+                    <h3 className="complain-input-lebel ">Category :</h3>
+                    <div className="mb-3 complain-input">
+                      <select type="text" className="form-control shadow-none complain-select" onChange={handleChange} value={formData.category} name="category" >
+                        <option value="" >Select Category</option>
+                        <option value="Category 1" >Category 1</option>
+                        <option value="Category 2" >Category 2</option>
+                        <option value="Category 3" >Category 3</option>
+                        <option value="Category 4" >Category 4</option>
+                        <option value="Category 5" >Category 5</option>
+                      </select>
+                    </div>
+                  </div>
+                  <div className="col lg-6 col-12">
+                    <h3 className="complain-input-lebel">Service :</h3>
+                    <div className="mb-3 complain-input">
+                      <select type="text" className="form-control shadow-none complain-select" onChange={handleChange} value={formData.service} name="service">
+                        <option value="" >Select Service</option>
+                        <option value="Service 1" >Service 1</option>
+                        <option value="Service 2" >Service 2</option>
+                        <option value="Service 3" >Service 3</option>
+                        <option value="Service 4" >Service 4</option>
+                        <option value="Service 5" >Service 5</option>
+                      </select>
+                    </div>
+                  </div>
+                </div>
+                <h3 className="complain-input-lebel">Complain Related Doc(if any) :</h3>
+                <div className="mb-3 complain-input">
+                  <input type="file" className="form-control shadow-none complain-input" onChange={e => setFile(e.target.files[0])} name="file" placeholder="No File Choose" />
+                </div>
+
+                <h3 className="complain-input-lebel">Description :</h3>
+                <div className="mb-3 complain-input">
+                  <textarea type="text" className="form-control shadow-none complain-input" onChange={handleChange} value={formData.description} name="description" rows="3" cols="50" placeholder="Max Words 200..."></textarea>
+                </div>
+                <div className="complain-button">
+                  <button type="submit">Submit</button>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </>
+  )
+}
+
+export default NonExistingClients

6f7b2849cb9f1a1566e3ac59d5fee03304b6bb03
Ashivkhan <ashivkhan347@gmail.com>
2024-03-05 18:05:51 +0530

    restroom_RXMemberships page added sidebar

--- src/pages/serviceDepartment/serviceDepartmentItem/RestroomClinicRXDepartment/RestroomRXMemberships.jsx
+++ src/pages/serviceDepartment/serviceDepartmentItem/RestroomClinicRXDepartment/RestroomRXMemberships.jsx
@@ -1,10 +1,19 @@
 import React from "react";
 import "./restroomClinicRXDepartment.css";
+import ImportantLinks from "../../../../components/ImportantLinks/ImportantLinks";
 
 const RestroomRXMemberships = () => {
+ 
   return (
     <>
-      <div className="container p-0 my-5">
+    <div className="container p-0">
+      <div className="row m-0 p-0 flex">
+        <div className="col-lg-3 col-md-3 col-12 p-0 service_menu">
+          <ImportantLinks />
+        </div>
+        <div className="col d-flex flex-column justify-content-center align-items-center">
+          <h1 className="heading1"> Restroom RXMemberships </h1>
+          <div className="container p-0">
         <div className="row p-0 title_card_shadow justify-content-center">
           <div className="col-3 p-0 rx_Layer_one​_absolute rx_Layer_one​"></div>
           <div className="col-3 p-0 rx_Layer_two_absolute rx_Layer_two"></div>
@@ -18,8 +27,27 @@ const RestroomRXMemberships = () => {
           </div>
         </div>
       </div>
+        </div>
+      </div>
+    </div>
+      
     </>
   );
 };
 
 export default RestroomRXMemberships;
+
+{/* <div className="container p-0 my-5">
+        <div className="row p-0 title_card_shadow justify-content-center">
+          <div className="col-3 p-0 rx_Layer_one​_absolute rx_Layer_one​"></div>
+          <div className="col-3 p-0 rx_Layer_two_absolute rx_Layer_two"></div>
+          <div className="col-3 p-0 rx_Layer_three_absolute rx_Layer_three"></div>
+          <div className="col-3 p-0 rx_Layer_four_absolute rx_Layer_four"></div>
+          <div className="col-6 col-sm-6 col-md-4 col-lg-5 col-xl-5 col-xxl-5 p-0 rotate_position_relative">
+            <h6 className="rx_Catastrophic​">Rx Catastrophic​</h6>
+            <h6 className="rx_Shield​​">Rx Shield​​</h6>
+            <h6 className="rx_Preferred​​​">Rx Preferred</h6>
+            <h6 className="rx_Checkup​">Rx Checkup</h6>
+          </div>
+        </div>
+      </div> */}

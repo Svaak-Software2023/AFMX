f6804d047e915b66380bf30643274a725bebe65f
Sachin Kumar <Kumar9891sachin@gmail.com>
2024-04-23 10:53:25 +0530

    Changes

--- src/pages/AFMXCompany/aboutCompany/AboutTheCompany.jsx
+++ src/pages/AFMXCompany/aboutCompany/AboutTheCompany.jsx
@@ -1,43 +1,96 @@
-import { img } from "react-lazy-load-image-component"
-import ImportantLinks from "../../../components/ImportantLinks/ImportantLinks"
-import "./about.css"
+import { img } from "react-lazy-load-image-component";
+import ImportantLinks from "../../../components/ImportantLinks/ImportantLinks";
+import "./about.css";
 
+// ABOUT THE COMPANTY DESCRIPTION
 function AboutTheCompany() {
-    return (
-        <>
-            <div className="container p-0 my-3">
-                <div className="row m-0 p-0 ">
-                    <div className="col-lg-3 col-md-3 col-0 p-0 service_menu">
-                        <ImportantLinks />
-                    </div>
-                    <div className=" col-lg-9 col-md-9 col-12 bg-white p-3 service_content">
-                        <div>
-                            <h3 className="heading">Who is America’s Finest Maintenance Company</h3>
-                        </div>
-                        <div className="my-3" style={{ textAlign: "justify" }}>
-                            <p>America&#39;s Finest Maintenance Company, LLC is a well-known janitorial and facility maintenance company in Florida. America&#39;s Finest Maintenance Company, LLC has integrated cleaning science and ongoing training into custodial operations. Our maintenance and cleaning technicians at America&#39;s Finest Maintenance Company, LLC are trained to detect and remove hidden and unseen contaminants such as fungus, viruses, and bacteria-based dirt. Once identified, our cleaning professionals use environmentally safe products developed by our company and based on building science solutions, which is in the best interests of both our customers and the environment. Our extensive experience in customer service has equipped us with the skills and knowledge to exceed your expectations.</p>
-                            <p>America&#39;s Finest Maintenance Company, LLC provides the advantages of a locally owned and operated company backed by a national network. Our highly trained, fully dedicated professionals take pride in delivering the highest standards of cleanliness and will treat your office or facility as if it were their own. We are proud to offer our proprietary Capture and Removal Cleaning system, which outperforms traditional cleaning processes by up to 55%. When it comes to running your business, we understand how important it is to choose a dependable, trustworthy service provider. And when it comes to cleaning, you want trusted professionals who will treat your facility like their own and deliver exceptional results.</p>
-                            <p>America&#39;s Finest Maintenance Company, LLC provides the advantages of a locally owned and operated company backed by a national network. Our highly trained, fully dedicated professionals take pride in delivering the highest standards of cleanliness and will treat your office or facility as if it were their own. We are proud to offer our proprietary Capture and Removal Cleaning system, which outperforms traditional cleaning processes by up to 55%. When it comes to running your business, we understand how important it is to choose a dependable, trustworthy service provider. And when it comes to cleaning, you want trusted professionals who will treat your facility like their own and deliver exceptional results.</p>
-                        </div>
-
-                        <div className="my-3">
-                            <h3 className="heading">Cleaning Science Technology</h3>
-                        </div>
-                        <div className="row m-0 p-0">
-                            <div className="col-lg-5 col-12">
-                            <img effect="blur" src="./images/about/clean-science.webp" alt="" className="img-fluid" />
-                            </div>
-                            <div className="col-lg-7 col-12">
-                                <div className="my-3" style={{ textAlign: "justify" }}>
-                                    <p>Cleaning science technology refers to the use of technological chemicals, State of the art equipment, and advancements to save lives and prevent disease spread. In all of our cleaning methods, we use Nano Tech.</p>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+  return (
+    <>
+      <div className="container p-0 my-3">
+        <div className="row m-0 p-0 ">
+          <div className="col-lg-3 col-md-3 col-0 p-0 service_menu">
+            <ImportantLinks />
+          </div>
+          <div className=" col-lg-9 col-md-9 col-12 bg-white p-3 service_content">
+            <div>
+              <h3 className="heading">
+                Who is America’s Finest Maintenance Company
+              </h3>
             </div>
-        </>
-    )
+            <div className="my-3" style={{ textAlign: "justify" }}>
+              <p>
+                America&#39;s Finest Maintenance Company, LLC is a well-known
+                janitorial and facility maintenance company in Florida.
+                America&#39;s Finest Maintenance Company, LLC has integrated
+                cleaning science and ongoing training into custodial operations.
+                Our maintenance and cleaning technicians at America&#39;s Finest
+                Maintenance Company, LLC are trained to detect and remove hidden
+                and unseen contaminants such as fungus, viruses, and
+                bacteria-based dirt. Once identified, our cleaning professionals
+                use environmentally safe products developed by our company and
+                based on building science solutions, which is in the best
+                interests of both our customers and the environment. Our
+                extensive experience in customer service has equipped us with
+                the skills and knowledge to exceed your expectations.
+              </p>
+              <p>
+                America&#39;s Finest Maintenance Company, LLC provides the
+                advantages of a locally owned and operated company backed by a
+                national network. Our highly trained, fully dedicated
+                professionals take pride in delivering the highest standards of
+                cleanliness and will treat your office or facility as if it were
+                their own. We are proud to offer our proprietary Capture and
+                Removal Cleaning system, which outperforms traditional cleaning
+                processes by up to 55%. When it comes to running your business,
+                we understand how important it is to choose a dependable,
+                trustworthy service provider. And when it comes to cleaning, you
+                want trusted professionals who will treat your facility like
+                their own and deliver exceptional results.
+              </p>
+              <p>
+                America&#39;s Finest Maintenance Company, LLC provides the
+                advantages of a locally owned and operated company backed by a
+                national network. Our highly trained, fully dedicated
+                professionals take pride in delivering the highest standards of
+                cleanliness and will treat your office or facility as if it were
+                their own. We are proud to offer our proprietary Capture and
+                Removal Cleaning system, which outperforms traditional cleaning
+                processes by up to 55%. When it comes to running your business,
+                we understand how important it is to choose a dependable,
+                trustworthy service provider. And when it comes to cleaning, you
+                want trusted professionals who will treat your facility like
+                their own and deliver exceptional results.
+              </p>
+            </div>
+
+            <div className="my-3">
+              <h3 className="heading">Cleaning Science Technology</h3>
+            </div>
+            <div className="row m-0 p-0">
+              <div className="col-lg-5 col-12">
+                <img
+                  effect="blur"
+                  src="./images/about/clean-science.webp"
+                  alt=""
+                  className="img-fluid"
+                />
+              </div>
+              <div className="col-lg-7 col-12">
+                <div className="my-3" style={{ textAlign: "justify" }}>
+                  <p>
+                    Cleaning science technology refers to the use of
+                    technological chemicals, State of the art equipment, and
+                    advancements to save lives and prevent disease spread. In
+                    all of our cleaning methods, we use Nano Tech.
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
-export default AboutTheCompany
+export default AboutTheCompany;
